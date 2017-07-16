8edd7f8736deb2732466177eafc0f6bc1c08bcf2
Eugene Shevchenko <eugenebalashov2012@gmail.com>
2017-07-17 00:10:16 +0300

    Finish

--- symfony/src/AppBundle/Controller/AdminController.php
+++ symfony/src/AppBundle/Controller/AdminController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use \JavierEguiluz\Bundle\EasyAdminBundle\Controller\AdminController as BaseAdminBundle;
 use JavierEguiluz\Bundle\EasyAdminBundle\Event\EasyAdminEvents;
+use Symfony\Component\HttpFoundation\Request;
 
 class AdminController extends BaseAdminBundle
 {
@@ -43,6 +44,56 @@ class AdminController extends BaseAdminBundle
 			'entity' => $entity,
 			'fields' => $fields,
 			'delete_form' => $deleteForm->createView(),
+		]);
+	}
+
+	public function editUserAction()
+	{
+		$this->dispatch(EasyAdminEvents::PRE_EDIT);
+		$id = $this->request->query->get('id');
+		$easyadmin = $this->request->attributes->get('easyadmin');
+		$entity = $easyadmin['item'];
+
+		if ($this->request->isXmlHttpRequest() && $property = $this->request->query->get('property'))
+		{
+			$newValue = 'true' === strtolower($this->request->query->get('newValue'));
+			$fieldsMetadata = $this->entity['list']['fields'];
+			if (!isset($fieldsMetadata[$property]) || 'toggle' !== $fieldsMetadata[$property]['dataType'])
+			{
+				throw new \RuntimeException(sprintf('The type of the "%s" property is not "toggle".', $property));
+			}
+//			$this->updateEntityProperty($entity, $property, $newValue);
+			//my
+			$entity->{$property} = $newValue;
+			$this->preUpdateEntity($entity);
+
+			return new Response((string)$newValue);
+		}
+		$fields = $this->entity['edit']['fields'];
+		$editForm = $this->createEditForm($entity, $fields);
+		if (!$this->isGranted('ROLE_SUPER_ADMIN'))
+		{
+			$editForm->remove('enabled');
+			$editForm->remove('roles');
+		}
+		$deleteForm = $this->createDeleteForm($this->entity['name'], $id);
+		$editForm->handleRequest($this->request);
+		if ($editForm->isValid())
+		{
+			$this->preUpdateUserEntity($entity);
+			$this->em->flush();
+			$refererUrl = $this->request->query->get('referer', '');
+
+			return !empty($refererUrl)
+				? $this->redirect(urldecode($refererUrl))
+				: $this->redirect($this->generateUrl('easyadmin', ['action' => 'show', 'entity' => $this->entity['name'], 'id' => $id]));
+		}
+
+		return $this->render($this->entity['templates']['edit'], [
+			'form'          => $editForm->createView(),
+			'entity_fields' => $fields,
+			'entity'        => $entity,
+			'delete_form'   => $deleteForm->createView(),
 		]);
 	}
 
